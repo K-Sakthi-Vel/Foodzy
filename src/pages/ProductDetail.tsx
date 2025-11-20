a6d3ac098b6af21ebe19147368c2d0e50e78d9aa
K SAKTHIVEL <k.sakthivel@revca.tech>
2025-11-20 17:22:22 +0530

    product details page updates

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import FilterOptions from '../components/FilterOptions';
 import ProductInfo from '../components/ProductInfo';
+import ProductDetailPopularProducts from '../components/ProductDetailPopularProducts';
 
 const ProductDetail: React.FC = () => {
   return (
-    <div className="flex flex-col items-center w-[100vw] ">
-      <div className='flex items-center justify-center bg-[#F53E32] h-[70px] w-[100vw]'>
-        <div className='flex justify-between items-center w-[1310px]' style={{ fontFamily: 'Poppins' }}>
-          <p className='text-[19px] text-[#FFFFFF]'>Product</p>
-          <p className='text-[14px] text-[#FFFFFF]'>Home - Product</p>
+    <div className="flex flex-col items-center w-[100vw]">
+      <div className="flex items-center justify-center bg-[#F53E32] h-[70px] w-full">
+        <div className="flex justify-between items-center w-[1310px]" style={{ fontFamily: 'Poppins' }}>
+          <p className="text-[19px] text-[#FFFFFF]">Product</p>
+          <p className="text-[14px] text-[#FFFFFF]">Home - Product</p>
         </div>
       </div>
       <div className="grid grid-cols-12 gap-8 w-[1310px] mt-10">
@@ -19,6 +20,7 @@ const ProductDetail: React.FC = () => {
           <ProductInfo />
         </div>
       </div>
+      <ProductDetailPopularProducts />
     </div>
   );
 };
